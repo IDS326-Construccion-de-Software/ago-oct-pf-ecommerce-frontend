46764f964befba8c533f83b7c79579e5ed821eb6
Uriel020 <urielrre@gmail.com>
2025-08-31 10:14:12 -0400

    delete shoppingcart and home changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,12 @@
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import Home from "./pages/Home.jsx";
+import Home from "./pages/Landing.jsx";
 import ShoppingCart from "./pages/ShoppingCart.jsx";
 import "./styles/App.css";   
 import homeIcon from "./assets/Icono the Revenge V2.svg";
 
 function App() {
   return (
-    <Router>
-      <div>
-        <nav className="navbar">
-          <Link to="/">
-            <img src={homeIcon} alt="Inicio" className="nav-icon" />
-          </Link>
-
-          <Link to="/carrito" className="cart-link">
-            Carrito
-          </Link>
-        </nav>
-
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/carrito" element={<ShoppingCart />} />
-        </Routes>
-      </div>
-    </Router>
+    ''
   );
 }
 
